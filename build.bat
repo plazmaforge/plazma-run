8a5ad0dad63f1e39ecac0acade5659657bcc4656
ohapon <ohapon@gmail.com>
2024-03-03 08:27:29 +0200

    Move lib files

--- build.bat
+++ build.bat
@@ -10,19 +10,19 @@ rem compile
 
 %CC% %CFLAGS% lib/strlib.cpp -o strlib.o
 %CC% %CFLAGS% lib/wstrlib.cpp -o wstrlib.o
-%CC% %CFLAGS% pathlib.cpp -o pathlib.o
-%CC% %CFLAGS% fslib.cpp -o fslib.o
-%CC% %CFLAGS% wclib.cpp -o wclib.o
-%CC% %CFLAGS% iolib.cpp -o iolib.o
-%CC% %CFLAGS% srhlib.cpp -o srhlib.o
-%CC% %CFLAGS% syscpu.cpp -o syscpu.o
-%CC% %CFLAGS% sysos.cpp -o sysos.o
-%CC% %CFLAGS% sysuser.cpp -o sysuser.o
-%CC% %CFLAGS% syslocale.cpp -o syslocale.o
-%CC% %CFLAGS% syslocale_os.cpp -o syslocale_os.o
-%CC% %CFLAGS% sysinfo.cpp -o sysinfo.o
-%CC% %CFLAGS% syslib.cpp -o syslib.o
-%CC% %CFLAGS% getopt.cpp -o getopt.o
+%CC% %CFLAGS% lib/pathlib.cpp -o pathlib.o
+%CC% %CFLAGS% lib/fslib.cpp -o fslib.o
+%CC% %CFLAGS% lib/wclib.cpp -o wclib.o
+%CC% %CFLAGS% lib/iolib.cpp -o iolib.o
+%CC% %CFLAGS% lib/srhlib.cpp -o srhlib.o
+%CC% %CFLAGS% lib/syscpu.cpp -o syscpu.o
+%CC% %CFLAGS% lib/sysos.cpp -o sysos.o
+%CC% %CFLAGS% lib/sysuser.cpp -o sysuser.o
+%CC% %CFLAGS% lib/syslocale.cpp -o syslocale.o
+%CC% %CFLAGS% lib/syslocale_os.cpp -o syslocale_os.o
+%CC% %CFLAGS% lib/sysinfo.cpp -o sysinfo.o
+%CC% %CFLAGS% lib/syslib.cpp -o syslib.o
+%CC% %CFLAGS% lib/getopt.cpp -o getopt.o
 
 %CC% %CFLAGS% run_dump.cpp -o run_dump.o
 %CC% %CFLAGS% run_echo.cpp -o run_echo.o
@@ -41,8 +41,6 @@ rem link
 %CC% %LDFLAGS% iolib.o strlib.o wstrlib.o syslocale.o syslocale_os.o syslib.o run_cat.o -o run-cat.exe
 %CC% %LDFLAGS% getopt.o strlib.o wstrlib.o syscpu.o sysos.o sysuser.o syslocale.o syslocale_os.o sysinfo.o syslib.o run_sysinfo.o -o run-sysinfo.exe %LIBS%
 %CC% %LDFLAGS% getopt.o strlib.o wstrlib.o pathlib.o wclib.o fslib.o iolib.o srhlib.o syscpu.o sysos.o sysuser.o syslocale.o syslocale_os.o sysinfo.o syslib.o run_find.o -o run-find.exe %LIBS%
-
 %CC% %LDFLAGS% getopt.o strlib.o wstrlib.o pathlib.o wclib.o fslib.o run_ls.o -o run-ls
-
 %CC% %LDFLAGS% strlib.o wstrlib.o pathlib.o wclib.o fslib.o syslocale.o syslocale_os.o syslib.o run_mkdir.o -o run-mkdir.exe
 %CC% %LDFLAGS% strlib.o wstrlib.o pathlib.o wclib.o fslib.o syslocale.o syslocale_os.o syslib.o run_rmdir.o -o run-rmdir.exe
--- build.sh
+++ build.sh
@@ -16,21 +16,23 @@ fi
 ############
 $CC $CFLAGS lib/strlib.cpp -o strlib.o
 $CC $CFLAGS lib/wstrlib.cpp -o wstrlib.o
+$CC $CFLAGS lib/pathlib.cpp -o pathlib.o
+$CC $CFLAGS lib/wclib.cpp -o wclib.o
+$CC $CFLAGS lib/fslib.cpp -o fslib.o
+$CC $CFLAGS lib/iolib.cpp -o iolib.o
+$CC $CFLAGS lib/srhlib.cpp -o srhlib.o
+$CC $CFLAGS lib/syscpu.cpp -o syscpu.o
+$CC $CFLAGS lib/sysos.cpp -o sysos.o
+$CC $CFLAGS lib/sysuser.cpp -o sysuser.o
+$CC $CFLAGS lib/syslocale.cpp -o syslocale.o
+$CC $CFLAGS lib/syslocale_os.cpp -o syslocale_os.o
+$CC $CFLAGS lib/sysinfo.cpp -o sysinfo.o
+$CC $CFLAGS lib/syslib.cpp -o syslib.o
+$CC $CFLAGS lib/getopt.cpp -o getopt.o
 
-$CC $CFLAGS pathlib.cpp -o pathlib.o
-$CC $CFLAGS wclib.cpp -o wclib.o
-$CC $CFLAGS fslib.cpp -o fslib.o
-$CC $CFLAGS iolib.cpp -o iolib.o
-$CC $CFLAGS srhlib.cpp -o srhlib.o
-$CC $CFLAGS syscpu.cpp -o syscpu.o
-$CC $CFLAGS sysos.cpp -o sysos.o
-$CC $CFLAGS sysuser.cpp -o sysuser.o
-$CC $CFLAGS syslocale.cpp -o syslocale.o
-$CC $CFLAGS syslocale_os.cpp -o syslocale_os.o
-$CC $CFLAGS sysinfo.cpp -o sysinfo.o
-$CC $CFLAGS syslib.cpp -o syslib.o
-$CC $CFLAGS getopt.cpp -o getopt.o
-$CC $CFLAGS test_getopt.cpp -o test_getopt.o
+if [[ "$OSTYPE" == "darwin"* ]]; then
+   $CC $CFLAGS lib/sysos_mac.mm -o sysos_mac.o
+fi
 
 $CC $CFLAGS run_dump.cpp -o run_dump.o
 $CC $CFLAGS run_echo.cpp -o run_echo.o
@@ -42,9 +44,7 @@ $CC $CFLAGS run_ls.cpp -o run_ls.o
 $CC $CFLAGS run_mkdir.cpp -o run_mkdir.o
 $CC $CFLAGS run_rmdir.cpp -o run_rmdir.o
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-   $CC $CFLAGS sysos_mac.mm -o sysos_mac.o
-fi
+$CC $CFLAGS test_getopt.cpp -o test_getopt.o
 
 # link
 ###########
@@ -53,10 +53,9 @@ $CC $LDFLAGS iolib.o strlib.o wstrlib.o syslocale.o syslocale_os.o syslib.o run_
 $CC $LDFLAGS iolib.o strlib.o wstrlib.o syslocale.o syslocale_os.o syslib.o run_cat.o -o run-cat
 $CC $LDFLAGS $OBJS getopt.o strlib.o wstrlib.o syscpu.o sysos.o sysuser.o syslocale.o syslocale_os.o sysinfo.o syslib.o run_sysinfo.o -o run-sysinfo
 $CC $LDFLAGS $OBJS getopt.o strlib.o wstrlib.o pathlib.o wclib.o fslib.o iolib.o srhlib.o syscpu.o sysos.o sysuser.o syslocale.o syslocale_os.o sysinfo.o syslib.o run_find.o -o run-find
-$CC $LDFLAGS getopt.o test_getopt.o -o test-getopt
-
 $CC $LDFLAGS getopt.o strlib.o wstrlib.o pathlib.o wclib.o fslib.o run_ls.o -o run-ls
-
 $CC $LDFLAGS strlib.o wstrlib.o pathlib.o wclib.o fslib.o syslocale.o syslocale_os.o syslib.o run_mkdir.o -o run-mkdir
 $CC $LDFLAGS strlib.o wstrlib.o pathlib.o wclib.o fslib.o syslocale.o syslocale_os.o syslib.o run_rmdir.o -o run-rmdir
+
+$CC $LDFLAGS getopt.o test_getopt.o -o test-getopt
 
