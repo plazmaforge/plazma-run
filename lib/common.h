4068b2b2e6b76974380ee5639f5593d36820de6a
ohapon <ohapon@gmail.com>
2024-03-14 22:05:19 +0200

    Add md5 lib

--- lib/common.h
+++ lib/common.h
@@ -1,6 +1,16 @@
 #ifndef PLAZMA_LIB_COMMON_H
 #define PLAZMA_LIB_COMMON_H
 
+#include <string.h>
+
 #include "error.h"
+
+void lib_platform_zeroize(void *buf, size_t len) {
+    if (len > 0) {
+        // TODO
+        memset(buf, 0, len);
+    }
+}
+
 
 #endif // PLAZMA_LIB_COMMON_H
---
+++ lib/md5.h
@@ -0,0 +1,182 @@
+/**
+ * \file md5.h
+ *
+ * \brief MD5 message digest algorithm (hash function)
+ *
+ * \warning   MD5 is considered a weak message digest and its use constitutes a
+ *            security risk. We recommend considering stronger message
+ *            digests instead.
+ */
+
+#ifndef PLAZMA_LIB_MD5_H
+#define PLAZMA_LIB_MD5_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if !defined(LIB_MD5_ALT)
+
+/**
+ * \brief          MD5 context structure
+ *
+ * \warning        MD5 is considered a weak message digest and its use
+ *                 constitutes a security risk. We recommend considering
+ *                 stronger message digests instead.
+ *
+ */
+typedef struct lib_md5_context_t {
+    uint32_t total[2];          /*!< number of bytes processed  */
+    uint32_t state[4];          /*!< intermediate digest state  */
+    unsigned char buffer[64];   /*!< data block being processed */
+}
+lib_md5_context_t;
+
+#else  /* LIB_MD5_ALT */
+#include "md5_alt.h"
+#endif /* LIB_MD5_ALT */
+
+/**
+ * \brief          Initialize MD5 context
+ *
+ * \param ctx      MD5 context to be initialized
+ *
+ * \warning        MD5 is considered a weak message digest and its use
+ *                 constitutes a security risk. We recommend considering
+ *                 stronger message digests instead.
+ *
+ */
+void lib_md5_init(lib_md5_context_t* ctx);
+
+/**
+ * \brief          Clear MD5 context
+ *
+ * \param ctx      MD5 context to be cleared
+ *
+ * \warning        MD5 is considered a weak message digest and its use
+ *                 constitutes a security risk. We recommend considering
+ *                 stronger message digests instead.
+ *
+ */
+void lib_md5_free(lib_md5_context_t* ctx);
+
+/**
+ * \brief          Clone (the state of) an MD5 context
+ *
+ * \param dst      The destination context
+ * \param src      The context to be cloned
+ *
+ * \warning        MD5 is considered a weak message digest and its use
+ *                 constitutes a security risk. We recommend considering
+ *                 stronger message digests instead.
+ *
+ */
+void lib_md5_clone(lib_md5_context_t* dst,
+                       const lib_md5_context_t* src);
+
+/**
+ * \brief          MD5 context setup
+ *
+ * \param ctx      context to be initialized
+ *
+ * \return         0 if successful
+ *
+ * \warning        MD5 is considered a weak message digest and its use
+ *                 constitutes a security risk. We recommend considering
+ *                 stronger message digests instead.
+ *
+ */
+int lib_md5_starts(lib_md5_context_t* ctx);
+
+/**
+ * \brief          MD5 process buffer
+ *
+ * \param ctx      MD5 context
+ * \param input    buffer holding the data
+ * \param ilen     length of the input data
+ *
+ * \return         0 if successful
+ *
+ * \warning        MD5 is considered a weak message digest and its use
+ *                 constitutes a security risk. We recommend considering
+ *                 stronger message digests instead.
+ *
+ */
+int lib_md5_update(lib_md5_context_t* ctx,
+                       const unsigned char *input,
+                       size_t ilen);
+
+/**
+ * \brief          MD5 final digest
+ *
+ * \param ctx      MD5 context
+ * \param output   MD5 checksum result
+ *
+ * \return         0 if successful
+ *
+ * \warning        MD5 is considered a weak message digest and its use
+ *                 constitutes a security risk. We recommend considering
+ *                 stronger message digests instead.
+ *
+ */
+int lib_md5_finish(lib_md5_context_t* ctx,
+                       unsigned char output[16]);
+
+/**
+ * \brief          MD5 process data block (internal use only)
+ *
+ * \param ctx      MD5 context
+ * \param data     buffer holding one block of data
+ *
+ * \return         0 if successful
+ *
+ * \warning        MD5 is considered a weak message digest and its use
+ *                 constitutes a security risk. We recommend considering
+ *                 stronger message digests instead.
+ *
+ */
+int lib_internal_md5_process(lib_md5_context_t* ctx,
+                                 const unsigned char data[64]);
+
+/**
+ * \brief          Output = MD5( input buffer )
+ *
+ * \param input    buffer holding the data
+ * \param ilen     length of the input data
+ * \param output   MD5 checksum result
+ *
+ * \return         0 if successful
+ *
+ * \warning        MD5 is considered a weak message digest and its use
+ *                 constitutes a security risk. We recommend considering
+ *                 stronger message digests instead.
+ *
+ */
+int lib_md5(const unsigned char *input,
+                size_t ilen,
+                unsigned char output[16]);
+
+#if defined(LIB_SELF_TEST)
+
+/**
+ * \brief          Checkup routine
+ *
+ * \return         0 if successful, or 1 if the test failed
+ *
+ * \warning        MD5 is considered a weak message digest and its use
+ *                 constitutes a security risk. We recommend considering
+ *                 stronger message digests instead.
+ *
+ */
+int lib_md5_self_test(int verbose);
+
+#endif /* LIB_SELF_TEST */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PLAZMA_LIB_MD5_H */
