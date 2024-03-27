65a6dde08c52cf31bc17a3f31b93a0048e57be63
ohapon <ohapon@gmail.com>
2024-03-27 15:04:16 +0200

    Update common

---
+++ lib/common.cpp
@@ -0,0 +1,18 @@
+
+#include "common.h"
+
+void lib_platform_zeroize(void* buf, size_t len) {
+    if (len <= 0) {
+        return;
+    }
+    // TODO
+    memset(buf, 0, len);
+}
+
+void lib_zeroize_and_free(void* buf, size_t len) {
+    if (!buf) {
+        return;
+    }
+    lib_platform_zeroize(buf, len);
+    /*lib*/ free(buf);
+}
