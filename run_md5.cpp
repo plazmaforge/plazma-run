72eceea237c14121e989482796fd1545d391fc42
ohapon <ohapon@gmail.com>
2024-06-10 12:28:54 +0300

    Refactored

--- run_md5.cpp
+++ run_md5.cpp
@@ -7,133 +7,211 @@
 #include "iolib.h"
 #include "md5.h"
 
+#define MD_NAME "MD5"
 #define MD_SIZE 16
 
-void usage() {
-    fprintf(stderr, "Usage: run-md5 -s string\n");
-    fprintf(stderr, "       run-md5 file ...\n");
+const int RUN_MD_BY_STRING = 1;
+const int RUN_MD_BY_FILE   = 2;
+
+typedef struct lib_md_config_t {
+    const char* app_name;
+    const char* md_name;
+    int md_size;
+    int mode;
+    bool is_title;
+    bool is_upper;
+}
+lib_md_config_t;
+
+
+void run_md_usage(lib_md_config_t* config) {    
+    //lib_fs_get_base_name(config->app_name)
+    fprintf(stderr, "Usage: %s [-tu] -s string | file ...\n", config->app_name);
 }
 
-void print_str(const char* str) {
-    printf("MD5 (\"%s\") = ", str);
+void print_title(lib_md_config_t* config, const char* title) {
+    if (!title) {
+        return;
+    }
+    printf("%s (\"%s\") = ", config->md_name, title);
 }
 
-void print_sum(unsigned char sum[MD_SIZE], int upper) {
-    for (int i = 0; i < MD_SIZE; i++) {
-        if (upper) {
-            printf("%02X", sum[i]);
-        } else {
-            printf("%02x", sum[i]);
-        }        
+void print_sum(lib_md_config_t* config, unsigned char sum[]) {
+    const char* fmt = config->is_upper  ? "%02X" : "%02x";
+    for (int i = 0; i < config->md_size; i++) {
+        printf(fmt, sum[i]);
     }
 }
 
-void print_result(const char* str, unsigned char sum[MD_SIZE], int mode, int upper) {
-    if (mode == 1) {
-        print_sum(sum, upper);
-    } else {
-        print_str(str);
-        print_sum(sum, upper);
+void print_result(lib_md_config_t* config, const char* title, unsigned char sum[]) {
+    if (config->is_title) {
+        print_title(config, title);
     }
+    print_sum(config, sum);
     printf("\n");
 }
 
-int run_md5_by_string(const char* str) {
-    if (!str) {
-        fprintf(stderr, "Error calculation md5: String is empty\n");
-        return 1;
-    }
-    int len = strlen(str);
-    unsigned char sum[MD_SIZE];
-    if (lib_md5((const unsigned char*) str, len, sum) != 0) {
-        fprintf(stderr, "Error calculation md5 for string: %s\n", str);
-        return 1;
-    }
-    print_result(str, sum, 1, 0);
-    return 0;
+void print_error(lib_md_config_t* config, const char* error) {
+    fprintf(stderr, "Error calculation %s for %s: %s\n", config->md_name, (config->mode == RUN_MD_BY_STRING ? "string" : "file"), error);
 }
 
-int run_md5_by_file(const char* data, size_t size) {
+int run_md_by_mode(lib_md_config_t* config, const char* file_name, const char* data, size_t size) {
     if (!data) {
-        fprintf(stderr, "Error calculation md5: Data is empty\n");
+        print_error(config, (config->mode == RUN_MD_BY_STRING ? "String is empty" : "Data is empty"));
         return 1;
     }
-    unsigned char sum[MD_SIZE];
+    unsigned char sum[config->md_size];
     if (lib_md5((const unsigned char*) data, size, sum) != 0) {
-        fprintf(stderr, "Error calculation md5 for file\n");
+        print_error(config, (config->mode == RUN_MD_BY_STRING ? data : file_name));
         return 1;
     }
-    print_result(data, sum, 1, 0);
+    print_result(config, (config->mode == RUN_MD_BY_STRING ? data : file_name), sum);
     return 0;
 }
 
-int run_md5_by_files(const char* data, size_t size) {
-    if (!data) {
-        fprintf(stderr, "Error calculation md5: Data is empty\n");
-        return 1;
-    }
-    unsigned char sum[MD_SIZE];
-    if (lib_md5((const unsigned char*) data, size, sum) != 0) {
-        fprintf(stderr, "Error calculation md5 for file(s)\n");
-        return 1;
-    }
-    print_result(data, sum, 1, 0);
-    return 0;
-}
+// int run_md_by_string(lib_md_config_t* config, const char* data, size_t size) {
+//     if (!data) {
+//         fprintf(stderr, "Error calculation %s: String is empty\n", config->md_name);
+//         return 1;
+//     }
+//     unsigned char sum[config->md_size];
+//     if (lib_md5((const unsigned char*) data, size, sum) != 0) {
+//         fprintf(stderr, "Error calculation %s for string: %s\n", config->md_name, data);
+//         return 1;
+//     }
+//     print_result(config, data, sum);
+//     return 0;
+// }
 
-int main(int argc, char* argv[]) {
+// int run_md_by_file(lib_md_config_t* config, const char* data, size_t size) {
+//     if (!data) {
+//         fprintf(stderr, "Error calculation %s: Data is empty\n", config->md_name);
+//         return 1;
+//     }
+//     unsigned char sum[config->md_size];
+//     if (lib_md5((const unsigned char*) data, size, sum) != 0) {
+//         fprintf(stderr, "Error calculation %s for file\n", config->md_name);
+//         return 1;
+//     }
+//     print_result(config, data, sum);
+//     return 0;
+// }
+
+// int run_md_by_files(lib_md_config_t* config, const char* data, size_t size) {
+//     if (!data) {
+//         fprintf(stderr, "Error calculation %s: Data is empty\n", config->md_name);
+//         return 1;
+//     }
+//     unsigned char sum[config->md_size];
+//     if (lib_md5((const unsigned char*) data, size, sum) != 0) {
+//         fprintf(stderr, "Error calculation %s for file(s)\n", config->md_name);
+//         return 1;
+//     }
+//     print_result(config, data, sum);
+//     return 0;
+// }
+
+int run_md(lib_md_config_t* config, int argc, char* argv[]) {
+
     if (argc < 2) {
-        usage();
+        run_md_usage(config);
         return 1;
     }
 
     bool error = false;
     int opt;
-    bool is_str = false;
-    char* str = NULL;
 
-    while ((opt = getopt(argc, argv, "s:")) != -1) {
+    config->mode = RUN_MD_BY_FILE;
+    config->is_title = false;
+    config->is_upper = false;
+
+    char* data = NULL;
+    size_t size = 0;
+
+    while ((opt = getopt(argc, argv, "tus:")) != -1) {
         switch (opt) {
+        case 't':
+            config->is_title = true;
+            break;
+        case 'u':
+            config->is_upper = true;
+            break;
         case 's':
-            is_str = true;
-            str = optarg;
+            config->mode = RUN_MD_BY_STRING;
+            data = optarg;
             break;
         case '?':
+            error = true;
+            break;
+        case ':':
             error = true;
             break;
         }
     }
 
     if (error) {
-        usage();
+        run_md_usage(config);
         return 1;
     }
 
-    if (is_str) {
-        return run_md5_by_string(str);
+    //printf("argc  : %i\n", argc);
+    //printf("optind: %i\n", optind); 
+
+    if (config->mode == RUN_MD_BY_STRING) {
+        size = strlen(data);
+        return run_md_by_mode(config, NULL, data, size);
     } else {
 
+        if (argc - optind < 1) {
+            //fprintf(stderr, "%s: Incorrect argument count\n", argv[0]);
+            run_md_usage(config);
+            return 1;
+        }
+
         const char* file_name;
-        char* data;
-        size_t size;
         error = 0;
 
-        for (int i = 1; i < argc; i++) {
+        for (int i = optind; i < argc; i++) {
             file_name = argv[i];
             size = 0;
             errno = 0;
             data = lib_io_read_bytes(file_name, size);
+
+            // ERROR
             if (errno != 0) {
+                if (data) {
+                    free(data);
+                }
                 error = 1;
-                fprintf(stderr, "run-md5: %s: %s\n", file_name, strerror(errno));                
+                fprintf(stderr, "%s: %s: %s\n", config->app_name, file_name, strerror(errno));
                 continue;
             }
-            if (run_md5_by_file(data, size) != 0) {
+
+            // NO DATA
+            if (!data) {
+                error = 1;
+                fprintf(stderr, "%s: %s: %s\n", config->app_name, file_name, "No data");
+                continue;                    
+            }
+
+            if (run_md_by_mode(config, file_name, data, size) != 0) {    
                 error = 1;
             }
+            free(data);
         }
 
         return error;
     }
+
+}
+
+int main(int argc, char* argv[]) {
+
+    lib_md_config_t config;
+    config.app_name = "run-md5";
+    config.md_name = MD_NAME;
+    config.md_size = MD_SIZE;
+
+    return run_md(&config, argc, argv);
 
 }
