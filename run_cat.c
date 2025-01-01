a839eb8f9f73a8fb170e058c0b171be668163bcf
ohapon <ohapon@gmail.com>
2025-01-01 14:24:29 +0200

    Fixed read data

--- run_cat.c
+++ run_cat.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
@@ -7,6 +6,80 @@
 #include "iolib.h"
 #include "wstrlib.h"
 #include "syslib.h"
+
+static int _proc_file(const char* file_name) {
+    char* data       = NULL;
+    size_t data_size = 0;
+
+    // read
+    int retval = lib_io_read_all_bytes(file_name, &data, &data_size);
+    if (retval < 0 || !data) {
+        fprintf(stderr, "%s: %s: No such file or directory\n", prog_name, file_name);
+        free(data);
+        data      = NULL;
+        data_size = 0;
+        return -1;
+    }
+
+    // write
+    for (int i = 0; i < data_size; i++) {
+        printf("%c", data[i]);
+    }
+
+    free(data);
+
+    return 0;
+}
+
+int run_cat(const char** file_names, size_t file_count) {
+
+    if (!file_names) {
+        return 1;
+    }
+
+    if (file_count == 0) {
+        return 0;
+    }
+
+    lib_sys_locale_init();
+
+    ////
+    const char* file_name = NULL;
+    for (int i = 0; i < file_count; i++) {
+
+        file_name = file_names[i];
+        if (!file_name) {
+            // error
+            continue;
+        }
+
+        _proc_file(file_name);
+
+    }
+    ////
+
+    lib_sys_locale_restore();
+
+    // char* total_data = NULL;
+    // size_t total_size = 0;
+    // int retval = lib_io_read_cat_bytes(file_names, file_count, &total_data, &total_size);
+    // if (retval != 0) {
+    //     fprintf(stderr, "%s: No such file or directory\n", prog_name);
+    //     free(total_data);
+    //     return 1;
+    // }
+    // if (!total_data) {
+    //     fprintf(stderr, "%s: No data\n", prog_name);
+    //     return 1;
+    // }
+    // lib_sys_locale_init();
+    // for (int i = 0; i < total_size; i++) {
+    //     printf("%c", total_data[i]);
+    // }
+    // lib_sys_locale_restore();
+
+    return 0;
+}
 
 void usage() {
     fprintf(stderr, "Usage: run-cat file ...\n");
@@ -28,29 +101,5 @@ int main(int argc, char* argv[]) {
         file_names[i - 1] = argv[i];
     }
 
-    char* total_data = NULL;
-    size_t total_size = 0;
-
-    //char* total_data = lib_io_read_cat_bytes(file_names, file_count, &total_size);
-    int retval = lib_io_read_cat_bytes(file_names, file_count, &total_data, &total_size);
-    if (retval != 0 || !total_data) {
-        free(total_data);
-        return 1;
-    }
-
-    lib_sys_locale_init(); // WIN32 fast output with setvbuf(?)
-    lib_io_buf_init();
-
-    // incorrect last chars: non printable char
-    //printf("%s", total_data);
-    //printf("%ls", lib_char_wchar(total_data));   // IMPORTANT for WIN32 
-    //wprintf(L"%ls", lib_char_wchar(total_data)); // IMPORTANT for WIN32
-
-    for (int i = 0; i < total_size; i++) {
-        printf("%c", total_data[i]);
-    }
-
-    lib_sys_locale_restore(); // Important for WIN32: The locale was changed for the terminal
-
-    return 0;
+    return run_cat(file_names, file_count);
 }
