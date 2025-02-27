87620cf15fa7b741e2883c06086b16a83f9e9eb9
ohapon <ohapon@gmail.com>
2025-02-27 15:00:11 +0200

    Implemented OCT/BIN output

--- run_ascii.c
+++ run_ascii.c
@@ -4,7 +4,14 @@
 
 #include "getopt.h"
 
-const char* NAME[32] = {
+#define LIB_TYPE_BIN 1 << 1
+#define LIB_TYPE_OCT 1 << 2
+#define LIB_TYPE_DEC 1 << 3
+#define LIB_TYPE_HEX 1 << 4
+
+#define LIB_TYPE_DEF LIB_TYPE_DEC
+
+const char* CHR[32] = {
     "NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL",
     "BS",  "HT",  "LF",  "VT",  "FF",  "CR",  "SO",  "SI",
     "DLE", "DC1", "DC2", "DC3", "DC4", "NAC", "SYN", "ETB",
@@ -18,14 +25,13 @@ static const char* _to_str(char c) {
     return str;
 }
 
-static const char* _to_name(char c) {
+static const char* _to_chr(char c) {
     if (c >= 0 && c <= 31) {
-        return NAME[c];
+        return CHR[c];
     }
     if (c == 127) {
         return "DEL";
     }
-    //return "";
     return _to_str(c);
 }
 
@@ -33,34 +39,51 @@ static bool _non_print(char c) {
     return ((c >= 0 && c <= 31) || c == 127);
 }
 
-static void _print_char_dec(char c) {
-    printf("%3d %-3s  ", c, _to_name(c));
-    //if (_non_print(c)) {
-    //    printf("%3d %-3s  ", c, _to_name(c));
-    //} else {
-    //    printf("%3d %c    ", c, c);
-    //}
+static char to_bin(char c, int i) {
+    return ( (c & (1 << i)) ? '1' : '0' );
 }
 
-static void _print_char_hex(char c) {
-    printf("%02X %-3s  ", c, _to_name(c));
-    //if (_non_print(c)) {
-    //    printf("%02X %-3s  ", c, _to_name(c));
-    //} else {
-    //    printf("%02X %c    ", c, c);
-    //}
-}
-
-static void _print_char(int type, char c) {
-    if (type == 1) {
-        _print_char_dec(c);
-    } else if (type == 2) {
-        _print_char_hex(c);
+static void to_bin_count(char* buf, char c, int count) {
+    int n = count - 1;
+    for (int i = n; i >= 0; --i) {
+        buf[n - i] = to_bin(c, i);
     }
 }
 
+static void to_bin8(char* buf, char c) {
+    to_bin_count(buf, c, 8);
+}
+
+static void _print_char_dec(char c) {
+    printf("%3d %-3s  ", c, _to_chr(c));
+}
+
+static void _print_char_hex(char c) {
+    printf("%02X %-3s  ", c, _to_chr(c));
+}
+
+static void _print_char(int type, char c) {
+    if (type & LIB_TYPE_DEC) {
+        printf("%3d ", c);
+    } 
+    if (type & LIB_TYPE_HEX) {
+        printf("%02X ", c);
+    }
+    if (type & LIB_TYPE_OCT) {
+        printf("%03o ", c);
+    }
+    if (type & LIB_TYPE_BIN) {
+        char buf[9];
+        buf[8] = '\0';
+        to_bin8(buf, c);
+        printf("%s ", buf);
+    }
+
+    printf("%-3s  ", _to_chr(c));
+}
+
 void usage() {
-    fprintf(stderr, "Usage: run-ascii [-d | -h]\n");
+    fprintf(stderr, "Usage: run-ascii [-dxob]\n");
 }
 
 int main(int argc, char* argv[]) {
@@ -68,17 +91,25 @@ int main(int argc, char* argv[]) {
     int error = 0;
     int opt;
 
-    int type = 1;
+    int type = 0; //LIB_TYPE_DEF;
 
-    while ((opt = lib_getopt(argc, argv, "dh")) != -1) {
+    while ((opt = lib_getopt(argc, argv, "dxob")) != -1) {
         switch (opt) {
         case 'd':
             /* Set DEC type */
-            type = 1;
+            type |= LIB_TYPE_DEC;
             break;
-        case 'h':
+        case 'x':
             /* Set HEX type */
-            type = 2;
+            type |= LIB_TYPE_HEX;
+            break;
+        case 'o':
+            /* Set OCT type */
+            type |= LIB_TYPE_OCT;
+            break;
+        case 'b':
+            /* Set BIN type */
+            type |= LIB_TYPE_BIN;
             break;
         case '?':
             error = 1;
@@ -87,6 +118,10 @@ int main(int argc, char* argv[]) {
             error = 1;
             break;
         }
+    }
+
+    if (type == 0) {
+        type = LIB_TYPE_DEF;
     }
 
     if (error) {
