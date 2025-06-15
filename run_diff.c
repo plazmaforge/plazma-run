23df77e946125916cde6c2cb44974a2bd2a091ef
ohapon <ohapon@gmail.com>
2025-06-15 16:12:11 +0300

    Add run_diff

---
+++ run_diff.c
@@ -0,0 +1,248 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "getopt.h"
+#include "clilib.h"
+
+bool _is_empty(char* str) {
+    return (!str || *str == '\0');
+}
+
+bool _is_blank(char* str) {
+    return (*str == ' ');
+}
+
+bool _is_tab(char* str) {
+    return (*str == '\t');
+}
+
+bool _is_space(char* str) {
+    return _is_blank(str) || _is_tab(str);
+}
+
+bool _is_comment(char* str) {
+    return (*str == '#');
+}
+
+bool _is_end(char* str) {
+    return (*str == '\r' || *str == '\n');
+}
+
+bool _is_valid(char* str) {
+    return !_is_empty(str) 
+        && !_is_tab(str) 
+        && !_is_end(str);
+}
+
+bool _is_no_data(char* str) {
+    if (!str) {
+        return true;
+    }
+    size_t len = strlen(str);
+    if (len == 0) {
+        return true;
+    }
+    char* chr = str;
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        if (*chr == ' ') {
+            count++;
+        }
+        chr++;
+    }
+    return count == len;
+}
+
+char* _to_case(int mode, char* str) {
+    if (!str) {
+        return str;
+    }
+    size_t len = strlen(str);
+    if (len == 0) {
+        return str;
+    }
+    char* chr = str;
+    for (int i = 0; i < len; i++) {
+        *chr = mode ? toupper(*chr) : tolower(*chr);
+        chr++;
+    }
+    return str;
+}
+
+/**
+ * Returns true if the line is ignore 
+ */
+bool _is_ignore_line(char* str) {
+    if (_is_empty(str)) {
+        return true;
+    }
+
+    if (_is_space(str) || _is_end(str)) {
+
+            //str++;
+            // Whitespace: incorrect line
+            return true;
+    } else if (_is_comment(str)) {
+            // Comment line (#): ignore line
+            return true;
+    } 
+
+    return false;
+}
+
+int _print_indent() {
+    return printf("  ");
+}
+
+int _print_line_separator() {
+    return printf("\n");
+}
+                
+int _print_name_comment(char* name) {
+    return printf(" /* %s */", name);
+}
+
+int _print_hex_comment(int hex) {
+    unsigned int u = (unsigned int) hex;
+    //return printf(" /* 0x%0*x */", 2, u);
+    return printf(" /* 0x%02x */", u);
+}
+
+static int _line_cmp(char* line1, size_t len1, char* line2, size_t len2) {
+    if (!line1 && !line2) {
+        fprintf(stderr, "line1, line2 are NULL\n");
+        return 0;
+    }
+    if (len1 != len2) {
+        fprintf(stderr, "line1: %lu, line2: %lu\n", len1, len2);
+        return 1;
+    }
+    size_t len = len1;
+    char* s1 = line1;
+    char* s2 = line2;
+    for (size_t i = 0; i < len; i++) {
+        if (*s1++ != *s2++) {
+            // /fprintf(stderr, "line1: %lu, line2: %lu\n", line1, line2);
+            return 1;
+        }
+    }
+    return  0;
+} 
+
+int run_diff(const char* file_name1, const char* file_name2) {
+
+    if (!file_name1 && !file_name2) {
+        fprintf(stderr, "%s: File name1 and name2 are empty\n", prog_name);
+        return 1;
+    }
+
+    if (!file_name1) {
+        fprintf(stderr, "%s: File name1 is empty\n", prog_name);
+        return 1;
+    }
+
+    if (!file_name2) {
+        fprintf(stderr, "%s: File name2 is empty\n", prog_name);
+        return 1;
+    }
+
+    FILE* file1 = fopen(file_name1, "r");
+
+    if (!file1) {
+        fprintf(stderr, "%s: %s: Unable to open file1\n", prog_name, file_name1);
+        return 1;
+    }
+
+    FILE* file2 = fopen(file_name2, "r");
+
+    if (!file2) {
+        fprintf(stderr, "%s: %s: Unable to open file2\n", prog_name, file_name2);
+        fclose(file1);
+        return 1;
+    }
+
+    char* line1   = NULL;
+    size_t cap1   = 0;
+    size_t len1   = 0;
+    ssize_t read1 = 0;
+
+    char* line2   = NULL;
+    size_t cap2   = 0;
+    size_t len2   = 0;
+    ssize_t read2 = 0;
+
+    while ( (read1 = getline(&line1, &cap1, file1)) != -1 ) {
+        //fprintf(stderr, "Retrived line of length: %lu, %lu \n", read1, len1);
+        //fprintf(stderr, "%s \n", line1);
+
+        read2 = getline(&line2, &cap2, file2);
+
+        if (read2 == -1) {
+            fprintf(stdout, "\n>>: %s\n", line1);
+            break;
+        }
+
+        len1 = read1;
+        len2 = read2;
+
+        if (_line_cmp(line1, len1, line2, len2) != 0) {
+            fprintf(stdout, "\n>>: %s\n", line1);
+            fprintf(stdout, "\n<<: %s\n", line2);
+        }
+    }
+
+    fclose(file1);
+    fclose(file2);
+
+    return 0;
+}
+
+void usage() {
+    fprintf(stderr, "Usage: run-diff file1 file2\n");
+}
+
+int main(int argc, char* argv[]) {
+
+    int min_arg = 2; // <file1> <file2>
+    if (argc < min_arg + 1) {
+        usage();
+        return 0;
+    }
+
+    prog_name = lib_cli_prog_name(argv);
+    int error = 0;
+    int opt;
+
+    // while ((opt = lib_getopt(argc, argv, "f:")) != -1) {
+    //     switch (opt) {
+    //     case 'f':
+    //         break;
+    //     case '?':
+    //         error = 1;
+    //         break;
+    //     case ':':
+    //         error = 1;
+    //         break;
+    //     }
+    // }
+
+    // if (error) {
+    //     usage();
+    //     return 0;
+    // }
+
+    // if (argc - optind < min_arg) {
+    //     fprintf(stderr, "%s: Incorrect argument count\n", prog_name);
+    //     usage();
+    //     return 0;
+    // }
+
+    char* file_name1 = argv[1];
+    char* file_name2 = argv[2];
+
+    return run_diff(file_name1, file_name2);
+
+}
