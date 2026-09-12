b0e1c8b6c2a515a0900e8faf6e1980a54efb7f10
unknown <ohapon@gmail.com>
2026-09-12 15:32:03 +0300

    Add comments

--- lib/intlib.h
+++ lib/intlib.h
@@ -3,10 +3,16 @@
 
 #include <stdint.h>
 
+/**
+ * Convert char value to uint8
+ */
 static uint8_t _u8(char value) {
     return (uint8_t) value;
 }
 
+/**
+ * Set 'hi' and 'lo' uint8 values to uint16
+ */
 static uint16_t _u16(uint8_t b1, uint8_t b2) {
     return (b1 << 8) | b2;
 }
@@ -14,10 +20,16 @@ static uint16_t _u16(uint8_t b1, uint8_t b2) {
 //  buf[0] = (ocode >> 8) & 0xFF;
 //  buf[1] = ocode & 0xFF;
 
+/**
+ * Get 'hi' uint8 value from uint16
+ */
 static uint8_t _u81(uint16_t value) {    
     return (value >> 8) & 0xFF;
 }
 
+/**
+ * Get 'lo' uint8 value from uint16
+ */
 static uint8_t _u82(uint16_t value) { 
     return value & 0xFF;
 }
